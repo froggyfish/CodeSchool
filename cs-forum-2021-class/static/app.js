c7ddd9603c2e2fec72e7318851d6d2e948c77e58
froggyfish <masterkl2003@gmail.com>
2021-07-02 11:57:24 -0600

    server is completely online and functional

--- cs-forum-2021-class/static/app.js
+++ cs-forum-2021-class/static/app.js
@@ -1,71 +1,134 @@
-var app = new Vue ({
-    el:"#app",
+var app = new Vue ( { 
+    el: "#app", 
     vuetify: new Vuetify(),
+    data:{
+        page:"blog",
+        drawer:false,
+        selected_category:"all",
+        categories:[
+            "all",
+            "clothing",
+            "hunting",
+            "books",
+            "cards",
+            "coins",
+            "keychains",
+            "comic books",
+            "misc.",
+        ],
+        threads:[],
 
-data:{
-    todos:[
-        {
-            name:"Get groceries",
-            description: "Go to this store blah blah",
-            done: false,
-            editing: false,
-            deadline: new Date().toLocaleDateString()
-        },
-        {
-            name:"Mow the lawn",
-            description:"N/A",
-            done: false,
-            editing: false, 
-            deadline:  new Date().toLocaleDateString()
-        },
-        {
-            name:"Clean the house",
-            description:"The kitchen and living room",
-            done: false,
-            editing: false,
-            deadline: new Date().toLocaleDateString()
-        },
-        {
-            name:"Take dog on walk",
-            description:"N/A",
-            done: false,
-            editing: false,
-            deadline: new Date().toLocaleDateString()
-        },
-    ],
-    new_todo_name: "",
-    new_todo_description: "",
-    new_todo_deadline: "",
+        postings:[],
 
+        //for a new thread
+        new_name:"",
+        new_author:"",
+        new_description:"",
+        new_category:"all",
+
+        //for a new post on a thread
+        new_post_body:"",
+        new_post_author:"",
+
+        server_url:"http://forum2021.codeschool.cloud"
+    }, 
+    created:function(){
+        this.getThreads();
     },
+    methods:{
+        getThreads:function(){
+            fetch(this.server_url+"/thread").then(function(res){
+                res.json().then(function(data){
+                    app.threads= data;
+                })
+            });
+        },
 
-
-methods:{
- 
-    addNewTodo: function(){
-            var new_todo = { 
-                name: this.new_todo_name,
-                description: this.new_todo_description,
-                done: false,
-                editing: false, 
-                deadline: this.new_todo_deadline
+        createThread:function(){
+            var new_thread={
+                name:this.new_name,
+                author:this.new_author,
+                description:this.new_description,
+                category:this.new_category,
             };
-            this.todos.unshift( new_todo );
-            this.new_todo = "";
-    },
-    
-    deleteTodo: function ( index ) {
-        this.todos.splice(index, 1);
-    },
+            fetch(this.server_url+"/thread",{
+                method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+                },
+                body:JSON.stringify(new_thread)
+            }).then(function() {
+                app.getThreads();
+                app.new_name="";
+                app.new_author="";
+                app.category="all";
+                app.new_description="";
+                app.page="blog";
+
+            });
+        },
+        deleteThread:function(thread_id){
+            fetch(this.server_url+"/thread/"+thread_id,{
+                method:"DELETE",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }).then(function(){
+                app.getThreads()})
+        },
+        
+        getPosts:function(thread_id){
+            fetch(this.server_url+"/thread/"+thread_id).then(function(res){
+                res.json().then(function(data){
+                    app.postings= data;
+                    console.log(data)
+                })
+            }).then(function(){
+                app.page="posts"
+            });
+        },
+
+        createPost:function(thread_id){
+            var new_post={
+                thread_id:thread_id,
+                author:this.new_post_author,
+                body:this.new_post_body
+            };
+            fetch(this.server_url+"/post",{
+                method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+                },
+                body:JSON.stringify(new_post)
+            }).then(function() {
+                app.getPosts(thread_id);
+                app.new_post_author="";
+                app.new_post_body="";
+
+            });
+        },
+        deletePost:function(post){
+            fetch(this.server_url+"/post/"+post.thread_id+"/"+post._id,{
+                method:"DELETE",
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }).then(function(){
+                app.getPosts(post.thread_id)})
+        }
 
 
-    editTodo: function(todo){
-        todo.editing = true;
-        // this.$set(todo, 'editing', true);
     },
-    saveTodo: function(todo){
-        todo.editing = false;
-
+    computed:{
+        sorted_threads:function(){
+            if(this.selected_category == "all"){
+                return this.threads;
+            } else {
+                var sorted_threads = this.threads.filter(function(thread){
+                    return thread.category == app.selected_category;
+                });
+                return sorted_threads;
+            }
+        }
     }
-}
 });
